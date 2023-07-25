e60b6128f4d9bf7516444dedb92b41b8a9091e32
Jonathas <jonatahas.azevedo@hotmail.com>
2023-07-25 13:42:23 -0300

    Creat sectio Sobre

---
+++ js/componets/main-carousel.js
@@ -0,0 +1,13 @@
+const btnRight = window.document.querySelector('.button-arrow.-right');
+const btnLeft = window.document.querySelector('.button-arrow.-left');
+const elements = window.document.querySelector('.elements');
+let pixels = 0;
+
+btnRight.addEventListener('click', function() {
+    pixels = pixels + 10;
+    elements.style = `transform: translateX(${pixels}px)`;
+});
+btnLeft.addEventListener('click', function() {
+    pixels = pixels - 10;
+    elements.style = `transform: translateX(${pixels}px)`;
+});
